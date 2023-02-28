f4c1bfba6170312d7f889a46ae85a1b047825fe7
monimaraver <monimaraver18@gmail.com>
2023-02-28 21:56:54 +0100

    doctor-CRUD

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
 const clinicaRouter = require('./clinica.route')
+const doctorRouter = require('./doctor.route')
 
 router.use('/clinicas', clinicaRouter)
+router.use('/doctor', doctorRouter)
 
 module.exports = router
